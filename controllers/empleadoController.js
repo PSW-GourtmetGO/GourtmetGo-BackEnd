96229ddfc3320c033395c797dd6f9741189553a1
Sebastian Ilbay <atlantiscityplay@gmail.com>
2024-05-05 21:33:56 -0500

    Implementación get empleado WEB
    Se ha implementado los métodos necesarios para obtener todos los empleados de un restaurante, y la información de un determinado empleado

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -23,3 +23,52 @@ exports.crearEmpleado = async (request, response) => {
       response.status(500).send('ERROR DURANTE EL PROCEDIMIENTO: CREAR EMPLEADO');
     }
   };
+
+exports.obtenerEmpleados = async (request, response) => {
+    try {
+        const { restaurante_id } = request.query;
+        let query = 'SELECT * FROM empleados WHERE restaurante_id = ?';
+
+        conexionBD.query(query, [restaurante_id], (err, empleados) => {
+            if (err) {
+                console.log(err);
+                response.status(500).send('ERROR DURANTE EL PROCEDIMIENTO: OBTENER EMPLEADOS');
+                return;
+            }
+            if (empleados.length === 0) {
+                response.status(404).send('NO EXISTEN EMPLEADOS');
+            } else {
+                console.log(empleados);
+                response.json(empleados);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        response.status(500).send('ERROR DURANTE EL PROCEDIMIENTO: OBTENER EMPLEADOS');
+    }
+};
+
+exports.obtenerEmpleado = async (request, response) => {
+  try {
+      const { empleado_id } = request.query;
+      let query = 'SELECT * FROM empleados WHERE id = ?';
+
+      conexionBD.query(query, [empleado_id], (err, empleado) => {
+          if (err) {
+              console.log(err);
+              response.status(500).send('ERROR DURANTE EL PROCEDIMIENTO: OBTENER EMPLEADO');
+              return;
+          }
+          if (empleado.length === 0) {
+              response.status(404).send('NO EXISTEN EL EMPLEADO');
+          } else {
+            const empleadoData = empleado[0];
+            const elEmpleado = new Empleado (empleadoData.id,empleadoData.cedula,empleadoData.nombre,empleadoData.apellido,empleadoData.correo,empleadoData.contrasenia,empleadoData.telefono,empleadoData.direccion);
+            response.json(elEmpleado);
+          }
+      });
+  } catch (error) {
+      console.log(error);
+      response.status(500).send('ERROR DURANTE EL PROCEDIMIENTO: OBTENER EMPLEADO');
+  }
+};
--- routes/empleadoRoute.js
+++ routes/empleadoRoute.js
@@ -3,4 +3,6 @@ const router = express.Router()
 const empleadoController = require('../controllers/empleadoController')
 
 router.post('/',empleadoController.crearEmpleado)
+router.get('/',empleadoController.obtenerEmpleados)
+router.get('/get',empleadoController.obtenerEmpleado)
 module.exports= router
