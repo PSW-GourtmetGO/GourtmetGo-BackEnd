df51626419f115aa0de2c3979225867af1ef5d14
Sebastian Ilbay <atlantiscityplay@gmail.com>
2024-06-03 23:16:27 -0500

    servicio platos movil
    servicio platos movil

--- controllers/generalMovilController.js
+++ controllers/generalMovilController.js
@@ -37,3 +37,61 @@ exports.obtenerRestaurante = async (request, response) => {
         response.status(500).json({ success: false, message: 'PROCESO FALLIDO RESTAURANTES' });
     }
 };
+
+exports.obtenerPlatosRestaurantes = async (request, response) => {
+    try {
+        const {restaurante} = request.query
+        let query = 'SELECT s.* FROM platos s, categorias c WHERE s.categoria_id=c.id AND c.restaurante_id=? AND c.eliminado="false" AND c.ver="true" AND s.eliminado ="false" AND s.ver="true"';
+
+        conexionBD.query(query, [restaurante],(err, restaurantes) => {
+            if (err) {
+                console.log(err);
+                response.status(500).json({ success: false, message: 'PROCESO FALLIDO PLATOS' });
+            }else {
+                response.status(200).json({ success: true, message: 'PROCESO EXITOSO PLATOS',restaurantes });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({ success: false, message: 'PROCESO FALLIDO PLATOS' });
+    }
+};
+
+exports.obtenerPlatosRestaurantesFiltro = async (request, response) => {
+    try {
+        const {restaurante,plato} = request.body
+        const nombrePlato = `%${plato}%`;
+        let query = 'SELECT s.* FROM platos s, categorias c WHERE s.categoria_id=c.id AND c.restaurante_id=? AND c.eliminado="false" AND c.ver="true" AND s.eliminado ="false" AND s.ver="true" AND s.nombre LIKE ?';
+
+        conexionBD.query(query, [restaurante,nombrePlato],(err, restaurantes) => {
+            if (err) {
+                console.log(err);
+                response.status(500).json({ success: false, message: 'PROCESO FALLIDO PLATOS FILTRO' });
+            }else {
+                response.status(200).json({ success: true, message: 'PROCESO EXITOSO PLATOS FILTRO',restaurantes });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({ success: false, message: 'PROCESO FALLIDO PLATOS FILTRO' });
+    }
+};
+
+exports.obtenerCategoriasRestaurantes = async (request, response) => {
+    try {
+        const {restaurante} = request.query
+        let query = 'SELECT * FROM categorias WHERE restaurante_id=? AND eliminado="false" AND ver="true"';
+
+        conexionBD.query(query, [restaurante],(err, restaurantes) => {
+            if (err) {
+                console.log(err);
+                response.status(500).json({ success: false, message: 'PROCESO FALLIDO CATEGORIAS' });
+            }else {
+                response.status(200).json({ success: true, message: 'PROCESO EXITOSO CATEGORIAS',restaurantes });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({ success: false, message: 'PROCESO FALLIDO CATEGORIAS' });
+    }
+};
--- routes/generalMovilRoutes.js
+++ routes/generalMovilRoutes.js
@@ -4,4 +4,7 @@ const controller = require('../controllers/generalMovilController')
 
 router.get('/restaurantes',controller.obtenerRestaurantes)
 router.post('/restaurantes',controller.obtenerRestaurante)
+router.get('/platos',controller.obtenerPlatosRestaurantes)
+router.post('/platos',controller.obtenerPlatosRestaurantesFiltro)
+router.get('/categorias',controller.obtenerCategoriasRestaurantes)
 module.exports= router
